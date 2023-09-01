f23dc412b5fc77e2237a830d7086228a731ba7ea
Patrick <cptty360@gmail.com>
2023-09-02 03:23:44 +1000

    EnemySpawning fixed and ENemy prefabs fixed

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,39 @@ using UnityEngine.AI;
 
 public class EnemyHealth : MonoBehaviour
 {
+    // Enemy health
     public int maxHealth = 3; // The maximum health of the enemy
     public int currentHealth; // The current health of the enemy
 
-    public List<MeshRenderer> meshColours;
+    public List<MeshRenderer> flashWhiteColour;
 
-    public Material regularMat;
     public Material hitMat;
+
+    public Material[] materialVariants; // Array of different material variants
+
+    private int currentMaterialIndex = 0; // Index of the currently selected material
+
+    private Renderer render; // Reference to the object's renderer
+    // Enum to represent different material variants
+    public enum MaterialVariant
+    {
+        Red,
+        Blue,
+        Yellow
+    }
+    public MaterialVariant currentMaterial;
+    public EnemyLevel enemyLevel;
+    public enum EnemyLevel
+    {
+        Red,
+        Blue,
+        Yellow
+    }
+
 
     public GameObject regularEnemy;
 
-    public float pauseDuration = 1f;
+
 
     public GameObject enemyAnimationController;
 
@@ -29,8 +52,11 @@ public class EnemyHealth : MonoBehaviour
 
     private NavMeshAgent navMeshAgent;
 
-    public float speedMultiplier = 2.0f;
 
+    public float speedMultiplier = 2.0f;
+    public float pauseDuration = 1f;
+
+    // All audio References 
     public AudioSource audioSource;  // Reference to the Audio Source
     public AudioClip hitSound;       // Sound to be played when hit
     public AudioSource audiobreak;  // Reference to the Audio Source
@@ -48,14 +74,73 @@ public class EnemyHealth : MonoBehaviour
         navMeshAgent = GetComponent<NavMeshAgent>();
         navMeshAgent.enabled = true;
 
+        //Get the material chosen
+        render = GetComponent<Renderer>();
+
+        currentMaterialIndex = (int)currentMaterial; // Set the index based on the currentMaterial enum value
+        UpdateMaterial(); // Set the initial material
+
+        //Enemy Health Level selection
+        SetMaxHealthFromEnum();
+        MaterialStartup();
+
+    }
+
+
+    private void UpdateMaterial()
+    {
+        switch (currentMaterial)
+        {
+            case MaterialVariant.Red:
+                render.material = materialVariants[0]; // Assuming the first material is red
+                break;
+            case MaterialVariant.Blue:
+                render.material = materialVariants[1]; // Assuming the second material is blue
+                break;
+            case MaterialVariant.Yellow:
+                render.material = materialVariants[2]; // Assuming the third material is yellow
+                break;
+        }
+    }
+
+    private void SetMaxHealthFromEnum()
+    {
+        switch (enemyLevel)
+        {
+            case EnemyLevel.Red:
+                currentHealth = 3; // Set the appropriate value for the Red level
+                break;
+            case EnemyLevel.Blue:
+                currentHealth = 2; // Set the appropriate value for the Blue level
+                break;
+            case EnemyLevel.Yellow:
+                currentHealth = 1; // Set the appropriate value for the Yellow level
+                break;
+            default:
+                currentHealth = 3; // Default value
+                break;
+        }
     }
 
     public void TakeDamage(int damageAmount)
     {
-        if (currentHealth >= 0)
+        if (currentHealth > 0)
         {
             currentHealth -= damageAmount; // Decrease the current health by the damage amount
-                                           // Play the hit sound
+
+            if (currentHealth == 2)
+            {
+                currentMaterial = MaterialVariant.Blue; // Change to blue variant
+                currentMaterialIndex = 1; // Update the index for the blue material
+            }
+            else if (currentHealth == 1)
+            {
+                currentMaterial = MaterialVariant.Yellow; // Change to yellow variant
+                currentMaterialIndex = 2; // Update the index for the yellow material
+            }
+            UpdateMaterial(); // Update the material based on the currentMaterial value
+
+            // Play the hit sound
             audioSource.PlayOneShot(hitSound);
         }
         // Check if the enemy has been defeated
@@ -77,8 +162,32 @@ public class EnemyHealth : MonoBehaviour
             // Turn on isKinematic while the animation plays
             Rigidbody enemyRigidbody = GetComponent<Rigidbody>();
             enemyRigidbody.isKinematic = true;
+
         }
     }
+
+    public void MaterialStartup()
+    {
+        switch (currentMaterial)
+        {
+            case MaterialVariant.Red:
+                currentMaterialIndex = 0;
+                break;
+            case MaterialVariant.Blue:
+                currentMaterialIndex = 1;
+                break;
+            case MaterialVariant.Yellow:
+                currentMaterialIndex = 2;
+                break;
+            default:
+                currentMaterialIndex = 0; // Default to Red if the material is not recognized
+                break;
+        }
+
+        // Set the material based on the currentMaterialIndex
+        render.material = materialVariants[currentMaterialIndex];
+    }
+
 
     private IEnumerator PauseBeforeDeathCoroutine()
     {
@@ -90,25 +199,30 @@ public class EnemyHealth : MonoBehaviour
     private void Die()
     {
         Debug.Log("Enemy has been defeated!");
-
+        navMeshAgent.isStopped = true;
         // Destroy the enemy GameObject
         Destroy(prefabGameObject);
     }
 
-    // Enemy hit Colour changer
     IEnumerator WaitForMatChange()
     {
-        foreach(MeshRenderer render in meshColours)
+        // Store the original materials
+        List<Material> originalMaterials = new List<Material>();
+        foreach (MeshRenderer renderer in flashWhiteColour)
         {
-            render.material = hitMat;
+            originalMaterials.Add(renderer.material);
+            renderer.material = hitMat;
         }
+
         yield return new WaitForSeconds(0.1f);
-        foreach (MeshRenderer render in meshColours)
+
+        // Restore the original materials and switch to the next material variant
+        for (int i = 0; i < flashWhiteColour.Count; i++)
         {
-            render.material = regularMat;
+            flashWhiteColour[i].material = materialVariants[currentMaterialIndex]; // Use the currentMaterialIndex to select the material from the array
         }
-        
     }
+
     public void ChangeEnemyMat()
     {
 
--- Assets/Scripts/EnemySpawning.cs
+++ Assets/Scripts/EnemySpawning.cs
@@ -5,7 +5,6 @@ using TMPro;
 
 public class EnemySpawning : MonoBehaviour
 {
-    public GameObject[] enemyPrefabs;
     public GameObject[] spawnPoints;
     public float timeBetweenWaves = 3.5f;
 
@@ -26,6 +25,18 @@ public class EnemySpawning : MonoBehaviour
 
     private float waveStartTime;
     private float currentWaveTimeLimit;
+
+    public enum EnemyLevel
+    {
+        Red,
+        Blue,
+        Yellow
+    }
+
+    public GameObject defaultObject;
+    public GameObject prefabOne;
+    public GameObject prefabTwo;
+    public GameObject prefabThree;
 
     private void Start()
     {
@@ -53,6 +64,21 @@ public class EnemySpawning : MonoBehaviour
             wavesText.text = "Wave: " + currentWave;
             waveWaitText.text = "Wave: " + currentWave;
             StartCoroutine(StartNewWaveWithDelay(timeBetweenWaves));
+
+        }
+
+
+        if (currentWave >= 1 && currentWave <= 4)
+        {
+            defaultObject = prefabOne;
+        }
+        else if (currentWave > 4 && currentWave <= 9)
+        {
+            defaultObject = prefabTwo;
+        }
+        else if (currentWave > 9 && currentWave <= 14)
+        {
+            defaultObject = prefabThree;
         }
     }
 
@@ -81,17 +107,81 @@ public class EnemySpawning : MonoBehaviour
     {
         enemiesInWave = currentWave;
         enemiesInWave *= 2;
-
-        for (int i = 0; i < enemiesInWave; i++)
+        if (currentWave <= 2)
         {
-            int randomPrefabIndex = Random.Range(0, enemyPrefabs.Length);
-            int randomSpawnIndex = Random.Range(0, spawnPoints.Length);
-            Vector3 spawnPosition = spawnPoints[randomSpawnIndex].transform.position;
-            Quaternion spawnRotation = spawnPoints[randomSpawnIndex].transform.rotation;
+            scoreTotal += 2;
+            for (int i = 0; i < enemiesInWave; i++)
+            {
+                // Use the defaultObject instead of random selection
+                GameObject selectedPrefab = prefabOne;
 
-            GameObject selectedPrefab = enemyPrefabs[randomPrefabIndex];
-            Instantiate(selectedPrefab, spawnPosition, spawnRotation);
+                int randomSpawnIndex = Random.Range(0, spawnPoints.Length);
+                Vector3 spawnPosition = spawnPoints[randomSpawnIndex].transform.position;
+                Quaternion spawnRotation = spawnPoints[randomSpawnIndex].transform.rotation;
+
+                Instantiate(selectedPrefab, spawnPosition, spawnRotation);
+            }
         }
+        else if (currentWave >= 3 && currentWave <= 5)
+        {
+            scoreTotal += 10;
+            for (int i = 0; i < enemiesInWave; i++)
+            {
+                // Select a random prefab between prefabOne and prefabTwo
+                GameObject selectedPrefab = (Random.Range(0, 2) == 0) ? prefabOne : prefabTwo;
+
+                int randomSpawnIndex = Random.Range(0, spawnPoints.Length);
+                Vector3 spawnPosition = spawnPoints[randomSpawnIndex].transform.position;
+                Quaternion spawnRotation = spawnPoints[randomSpawnIndex].transform.rotation;
+
+                Instantiate(selectedPrefab, spawnPosition, spawnRotation);
+            }
+        }
+        else if (currentWave >= 6 && currentWave <= 8)
+        {
+            scoreTotal += 15;
+            for (int i = 0; i < enemiesInWave; i++)
+            {
+                // Select a random prefab between prefabOne and prefabTwo
+                GameObject selectedPrefab = (Random.Range(0, 2) == 0) ? prefabThree : prefabTwo;
+
+                int randomSpawnIndex = Random.Range(0, spawnPoints.Length);
+                Vector3 spawnPosition = spawnPoints[randomSpawnIndex].transform.position;
+                Quaternion spawnRotation = spawnPoints[randomSpawnIndex].transform.rotation;
+
+                Instantiate(selectedPrefab, spawnPosition, spawnRotation);
+            }
+        }
+        else if (currentWave > 9)
+        {
+            scoreTotal += 20;
+            for (int i = 0; i < enemiesInWave; i++)
+            {
+                // Select a random prefab between prefabOne, prefabTwo, and prefabThree
+                int randomPrefabIndex = Random.Range(0, 3);
+                GameObject selectedPrefab;
+
+                if (randomPrefabIndex == 0)
+                {
+                    selectedPrefab = prefabOne;
+                }
+                else if (randomPrefabIndex == 1)
+                {
+                    selectedPrefab = prefabTwo;
+                }
+                else
+                {
+                    selectedPrefab = prefabThree;
+                }
+
+                int randomSpawnIndex = Random.Range(0, spawnPoints.Length);
+                Vector3 spawnPosition = spawnPoints[randomSpawnIndex].transform.position;
+                Quaternion spawnRotation = spawnPoints[randomSpawnIndex].transform.rotation;
+
+                Instantiate(selectedPrefab, spawnPosition, spawnRotation);
+            }
+        }
+
         currentWave++;
 
         // Update wave timer and time limit
@@ -108,17 +198,16 @@ public class EnemySpawning : MonoBehaviour
     {
         float waveCompletionTime = Time.time - waveStartTime;
 
-        if (waveCompletionTime <= currentWaveTimeLimit)
-        {
-            scoreTotal += 100;
-        }
-        else
-        {
-            float timeDifference = waveCompletionTime - currentWaveTimeLimit;
-            int timeScore = Mathf.Max(0, Mathf.FloorToInt(100 - (timeDifference * 10) - (currentWave * 5)));
-            scoreTotal += timeScore;
-        }
 
+        float timeDifference = waveCompletionTime - currentWaveTimeLimit;
+        int timeScore = Mathf.Max(0, Mathf.FloorToInt(100 - (timeDifference * 10) - (currentWave * 5)));
+        scoreTotal += timeScore;
+
+        if (currentWave == 2)
+        {
+            scoreTotal = 0;
+            scoreText.text = "Score: " + scoreTotal.ToString();
+        }
         scoreText.text = "Score: " + scoreTotal.ToString();
     }
 }
